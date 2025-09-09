0bc52227b879dab05e17567fe8d7777b1ef31312
Lukas Li <103869972+eatingfood142434@users.noreply.github.com>
2025-09-08 20:07:56 -0400

    Time: 0 ms (100.00%) | Memory: 9.3 MB (13.22%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -4,9 +4,11 @@ public:
         vector<int> ret;
         string s1, s2;
         for (int i = 1; i <= n/2; i++) {
-        s1 = to_string(i);
-        s2 = to_string(n-i);
-        if (!(s1.find('0') != string::npos) && !(s2.find('0') != string::npos)) ret = {i,n-i};
+            s1 = to_string(i);
+            s2 = to_string(n-i);
+            if (!(s1.find('0') != string::npos) && !(s2.find('0') != string::npos)) {ret = {i,n-i};
+                return ret;
+            }
         }
         return ret;
     }
