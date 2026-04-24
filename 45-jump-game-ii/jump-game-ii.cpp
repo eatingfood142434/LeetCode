5ba8b4eb001dfb45f3d6514d78f46357c0d135b2
Lukas Li <103869972+eatingfood142434@users.noreply.github.com>
2026-04-24 03:11:12 -0400

    Time: 0 ms (100.00%) | Memory: 20.4 MB (83.13%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n,-1);
-        dp[0] = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j <= nums[i]; j++) {
-                if (i+j < n) dp[i+j] = (dp[i+j] == -1) ? dp[i]+1 : min(dp[i+j],dp[i]+1);
-            }
+        int n = 0, f = 0, fa = 0, ret = 0;
+        while (f < nums.size()-1) {
+            fa = 0;
+            for (int i = n; i <= f; i++) fa = max(fa, i+nums[i]);
+            n = f;
+            f = fa;
+            ret++;
         }
-        return dp[n-1];
+        return ret;
     }
 };
